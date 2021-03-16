154797fb3aaf7b4b592640ddabbc2b81e8d23dae
misafric <fricovam4@gmail.com>
2021-03-16 20:08:39 +0100

    nav

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD9lsmZWhDU2t_J-lhp5au9fxpbd6KMgrc",
+    authDomain: "react-netflix-9bf22.firebaseapp.com",
+    projectId: "react-netflix-9bf22",
+    storageBucket: "react-netflix-9bf22.appspot.com",
+    messagingSenderId: "103600110617",
+    appId: "1:103600110617:web:9b24f047108396311dfe88"
+  };
